ed09db50c7b4434ac5e20c1b5d9763b48068578e
Mikaela Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2025-10-10 20:09:01 +0800

    complete doc routes

--- backend/src/controller/doctor.controller.js
+++ backend/src/controller/doctor.controller.js
@@ -41,3 +41,54 @@ export const fetchDoctorById = async (req,res) => {
         res.status(500).json({message: "Server error"})
     }
 }
+
+export const updateDoctorData = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updates = req.body;
+
+    
+    if (updates.password) {
+      updates.password = await bcrypt.hash(updates.password, 10);
+    }
+
+    const updatedDoctor = await Doctor.findByIdAndUpdate(id, updates, {
+      new: true,
+      runValidators: true,
+    }).select("-password"); 
+
+    if (!updatedDoctor) {
+      return res.status(404).json({ message: "Doctor not found" });
+    }
+
+    res.status(200).json({
+      message: "Doctor updated successfully",
+      doctor: updatedDoctor,
+    });
+  } catch (error) {
+    console.error("Update doctor error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const deleteDoctorAcc = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const doctor = await Doctor.findById(id).select("-password");
+
+    if (!doctor) {
+      return res.status(404).json({ message: "Doctor not found" });
+    }
+
+    await Doctor.findByIdAndDelete(id);
+
+    return res.status(200).json({
+      message: "Doctor account deleted successfully",
+      doctor,
+    });
+  } catch (error) {
+    console.error("Delete doctor error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- backend/src/routes/doctor.route.js
+++ backend/src/routes/doctor.route.js
@@ -1,14 +1,14 @@
 import express from "express"
 import verifyToken from "../middleware/verifyToken.js";
 import authorizeRoles from "../middleware/authorizeRoles.js";
-import { fetchAllDoctors, fetchDoctorById } from "../controller/doctor.controller.js";
+import { deleteDoctorAcc, fetchAllDoctors, fetchDoctorById, updateDoctorData } from "../controller/doctor.controller.js";
 
 
 const router = express.Router();
 
 router.get("/",  verifyToken, authorizeRoles("admin", "patient"), fetchAllDoctors);    // get all doctors
 router.get("/:id", verifyToken, authorizeRoles("admin", "doctor", "patient"), fetchDoctorById); // get specific doctor using id
-// router.put("/:id"); // update doctor profile
-// router.delete("/:id"); // delete 
+router.put("/:id", verifyToken, authorizeRoles("admin", "doctor"), updateDoctorData); // update doctor profile
+router.delete("/:id", verifyToken, authorizeRoles("admin", "doctor"), deleteDoctorAcc); // delete doctor account 
 
 export default router;
