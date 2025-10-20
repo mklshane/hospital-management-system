52ad742768cf4f04eedf8a9f7e4f45ad0c6ed0a3
luisantonioj <luisantoniojopia@gmail.com>
2025-10-20 20:08:18 +0800

    feat: route for appointments

--- src/layouts/DoctorLayout.jsx
+++ src/layouts/DoctorLayout.jsx
@@ -50,8 +50,8 @@ export default function DoctorLayout({ children }) {
       ),
     },
     {
-      label: "Profile",
-      href: "/profile",
+      label: "Appointments",
+      href: "/doctor/appointments",
       icon: (
         <IconUserBolt className="h-5 w-5 min-w-5 flex-shrink-0 text-neutral-700 dark:text-neutral-300" />
       ),
