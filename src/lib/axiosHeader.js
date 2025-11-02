20e9ab49c0157f6d5520dfad8ce44b8ae9499965
Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2025-11-03 05:17:03 +0800

    Update BASE_URL to production API endpoint

--- src/lib/axiosHeader.js
+++ src/lib/axiosHeader.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
+
+const BASE_URL = "https://final-project-group1-webdevt-backend.onrender.com/api";
 
 
 const axiosHeader = axios.create({
