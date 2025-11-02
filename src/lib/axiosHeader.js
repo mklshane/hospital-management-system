5c2ba741892ec0df020111a791b2bff9d21f9c51
Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2025-11-02 22:30:21 +0800

    update base url for deployment

--- src/lib/axiosHeader.js
+++ src/lib/axiosHeader.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api";
-
+const BASE_URL = "https://final-project-group1-webdevt-backend.onrender.com";
+ /* "http://localhost:3000/api"; */
 const axiosHeader = axios.create({
   baseURL: BASE_URL,
   timeout: 30000,
