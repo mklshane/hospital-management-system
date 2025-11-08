837b5134d05e8f36ca72176af4c165818f6cb907
Mikaela Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2025-11-08 23:24:16 +0800

    style: add margin bottom in doctor list

--- src/pages/Admin/DoctorsList.jsx
+++ src/pages/Admin/DoctorsList.jsx
@@ -115,7 +115,7 @@ const DoctorsList = () => {
           <div className="flex items-center gap-4">
             <button
               onClick={handleCreateDoctor}
-              className="bg-blue hover:bg-blue/90 text-white px-4 py-2 rounded-lg shadow transition flex items-center gap-2"
+              className="bg-blue hover:bg-blue/90 text-white px-4 py-2 rounded-lg shadow transition flex items-center gap-2 mb-2"
             >
               <svg
                 className="w-4 h-4"
