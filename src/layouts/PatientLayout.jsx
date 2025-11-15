8b3f75c11778c493774e1c49dd3c91e0aebcaf98
Mikaela Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2025-11-16 00:22:53 +0800

    style: improve PatientLayout responsiveness and navigation UX

--- src/layouts/PatientLayout.jsx
+++ src/layouts/PatientLayout.jsx
@@ -59,7 +59,7 @@ export default function DashboardLayout({ children }) {
       ),
     },
     {
-      label: "Appointments",
+      label: "Book an Appointment",
       href: "/appointments",
       icon: (
         <IconUserBolt className="h-5 w-5 min-w-5 flex-shrink-0 text-neutral-700 dark:text-neutral-300" />
